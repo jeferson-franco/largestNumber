937e641bcf7c774d52764d50914611ca8529b5e6
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-14 13:41:59 -0300

    feat: create largestNumber.js

---
+++ src/largestNumber.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    return parseInt('9'.repeat(n));
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test largestNumber
+
+// alternative solution
